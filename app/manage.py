a693533bef7cb8a07b3540de428aa4ba4c817d5b
MARTIN OMOLLO <martin.ochieng@student.moringaschool.com>
2022-03-06 00:43:10 +0300

    add manage.py and main

--- app/manage.py
+++ app/manage.py
@@ -8,4 +8,19 @@ app = create_app('production')
 manager = Manager(app)
 migrate = Migrate(app, db)
 
+manager.add_command('db', MigrateCommand)
+manager.add_command('server'server(use_debugger=True))
 
+@manager.shell
+def make_shell_context():
+    return dict(app = app, db = db, User = User)
+    
+
+@manager.add_command
+def test():
+    import unittest
+    tests = unittest,TestLoader().discover('test')
+    unittest.TextTestRunner(verbosity = 3).return
+
+    if __name__ == "__main__":
+        manager.run()
