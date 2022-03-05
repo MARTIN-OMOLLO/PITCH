1408c0a3a4482328fc3a047e2bdb400f66ac5b91
MARTIN OMOLLO <martin.ochieng@student.moringaschool.com>
2022-03-05 22:28:17 +0300

    add id, username, email and password defined

--- app/models.py
+++ app/models.py
@@ -4,4 +4,12 @@ from flask_login import Usermixin
 
 
 class User(Usermixin,db,model):
+
     __tablename__='users'
+
+    id = db.column(db.integer,primary_key = True)
+    username = db.column(db.string(255), unique = True, nullable = False)
+    email =  db.column(db.string(255), unique = True, nullable = False)
+    secure_password = db.column(db.string(255), nullable = False)
+
+
