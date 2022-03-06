890981faf50bae1dea8a509680d37989567bade8
MARTIN OMOLLO <martin.ochieng@student.moringaschool.com>
2022-03-06 08:23:20 +0300

    add validations

--- app/form.py
+++ app/form.py
@@ -12,3 +12,16 @@ class LoginForm(FlaskForm):
 
 class RegisterForm(FlaskForm):
     email = StringField('Your Email Address', validators=[Required()])
+    username = StringField('Enter Your username', validators =[Required()])
+    password = PasswordField('Password', validators[Required()])
+    password_confirm PasswordField('Cofirm Password', validators = [Required()])
+    Submit = SubmitField('Sign Up')
+
+def validate_email(self,data_field):
+    if user.query.filter_by(email = data_field.data).first():
+        raise ValidateionError("Email Exists")
+
+
+def validate_username(self, data_field):
+    if User.query.filter_by(username = data_field.data).first()
+    raise ValidateionError("Username exists")
