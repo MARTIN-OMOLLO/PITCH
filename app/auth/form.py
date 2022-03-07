e9fd2398fdec613bcd6b24f53887f12c36a26777
MARTIN OMOLLO <martin.ochieng@student.moringaschool.com>
2022-03-07 10:41:54 +0300

    add loginform

--- app/auth/form.py
+++ app/auth/form.py
@@ -23,3 +23,9 @@ class RegistrationForm(FlaskForm):
             #....
 from wtforms import StringField,PasswordField,BooleanField,SubmitField
 .....
+
+class LoginForm(FlaskForm):
+    email = StringField('Your Email Address',validators=[Required(),Email()])
+    password = PasswordField('Password',validators =[Required()])
+    remember = BooleanField('Remember me')
+    submit = SubmitField('Sign In')
