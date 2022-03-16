75f98ff2401f14843edbbb8f721b8d6965b64731
MARTIN OMOLLO <martin.ochieng@student.moringaschool.com>
2022-03-16 14:54:36 +0300

    add test

--- config.py
+++ config.py
@@ -1,38 +1,39 @@
-    Development  configuration child class
+import os
+# import re
+# from dotenv import load_dotenv
+# load_dotenv()
 
-    Args:
-        Config: The parent configuration class with General configuration settings
-    '''
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://martin:martin123@localhost/pitches'
-    DEBUG = True
-
-config_options = {
-'development':DevConfig,
-'production':ProdConfig
-}mport os
 class Config:
-    '''
-    General configuration parent class
-    '''
-    SECRET_KEY = 'pitches'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    @staticmethod
-    def init_app(app):
-        pass
-    
+  SQLALCHEMY_TRACK_MODIFICATIONS=True
+  SECRET_KEY = os.environ.get('SECRET_KEY')
 
+  #Simplemde cofigurations
+  SIMPLEMDE_JS_IIFE = True
+  SIMPLEMDE_USE_CDN = True
 
+  #Email configurations
+  MAIL_SERVER = 'smtp.googlemail.com'
+  MAIL_PORT = 587
+  MAIL_USE_TLS = True
+  MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+  MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+  #PHOTOS UPLOAD CONFIGURATION
+  UPLOADED_PHOTOS_DEST = 'app/static/photos'
 
 class ProdConfig(Config):
-    '''
-    Production  configuration child class
-
-    Args:
-        Config: The parent configuration class with General configuration settings
-    '''
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATA_URL')
-    
-
+  SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+  # if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+  #   SQLALCHEMY_DATABASE_URI =SQLALCHEMY_DATABASE_URI.replace("postgres://","postgresql://",1)
 
 class DevConfig(Config):
-    '''
+  SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://martin:MARtin1999@localhost/pitch'
+  DEBUG = True
+
+class TestConfig(Config):
+  SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://martin:MARtin1999@localhost/pitch_test'
+
+config_options = {
+  'production':ProdConfig,
+  'development':DevConfig,
+  'test':TestConfig
+}
--- manage.py
+++ manage.py
@@ -1,27 +1,26 @@
-
-from app import create_app, db
-from app.models import User
 from flask_script import Manager, Server
 from flask_migrate import Migrate, MigrateCommand
-app = create_app('production')
+from app import create_app,db
+from app.models import User
+
+app = create_app('development')
 
 manager = Manager(app)
+manager.add_command('server',Server)
 
-manager.add_command('server', Server)
-migrate = Migrate(app, db)
-manager.add_command('db', MigrateCommand)
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
 
 
 @manager.shell
 def make_shell_context():
-    return dict(app = app, db = db, User = User)
-    
+    return dict(app = app,db = db,User = User)
 
-# @manager.add_command
-# def test():
-#     import unittest
-#     tests = unittest,TestLoader().discover('test')
-    # unittest.TextTestRunner(verbosity = 3).return
+@manager.command
+def test():
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
 
 if __name__ == "__main__":
-     manager.run()
+    manager.run()
