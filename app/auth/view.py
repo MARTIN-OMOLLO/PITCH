0ac1905fa703ab237cc75cafe7328a04d730c9ae
MARTIN OMOLLO <martin.ochieng@student.moringaschool.com>
2022-03-17 01:35:37 +0300

    add macros

--- app/auth/view.py
+++ app/auth/view.py
@@ -1,48 +1,38 @@
-from flask import render_template,redirect,url_for
+from flask import render_template,redirect,url_for,flash,request
 from . import auth
-from flask_login import login_user, login_required, logout_user
-from .form import LoginForm, RegistrationForm
+from flask_login import login_user,logout_user,login_required
 from ..models import User
-from flask_login import login_user
-# from ..email import mail_message
+from .forms import LoginForm,RegistrationForm
 from .. import db
 
-# ....
-@auth.route('/login',methods = ["GET","POST"])
+
+@auth.route('/login',methods=['GET','POST'])
 def login():
     login_form = LoginForm()
-    if login_form.validate_on_submit:
-        user = User.query.filter_by(email = login_form.email.data,password = login_form.password.data)
-        if user is not None and login_form.password == login_form.password.data:
+    if login_form.validate_on_submit():
+        user = User.query.filter_by(email = login_form.email.data).first()
+        if user is not None and user.verify_password(login_form.password.data):
             login_user(user,login_form.remember.data)
-            db.session.add(user)
-            flash ('Username and Password are invalid')
-            return redirect(url_for('auth.login'))
-    title = "New Account"
-    return render_template('auth/login.html',login_form= login_form)
-
-
-# @auth.route('/signup',methods = ["GET","POST"])
-# def signup():
-#     signup_form = RegistrationForm()
-#     if signup_form.validate_on_submit:
-#         user = User.query.filter_by(email = signup_form.email.data, username = signup_form.username.data,password = signup_form.password.data)
-#         print("the user instance", user)
-#         return redirect(url_for('auth.login'))
-#     return render_template('signup.html',registrationform= signup_form)
-
-@auth.route('/signup', methods = ["GET","POST"])
-def signup():
-    signup_form = RegistrationForm()
-    return render_template('auth/signup.html', RegistrationForm = signup_form)
-    
-
-
-@auth.route('/logout',methods = ["GET","POST"])
+            return redirect(request.args.get('next') or url_for('main.index'))
+        flash('Invalid username or Password')
+    title = "pitchapp login"
+    return render_template('auth/login.html',login_form = login_form,title=title)
+        
+@auth.route('/logout')
+@login_required
 def logout():
-    login_form = LoginForm()
-    if login_form.validate_on_submit:
-        user = User.query.filter_by(email = form.email.data, username = login_form.username.data,password = login_form.password.data)
-        logout_user(user,login_form.remember.data)
+    logout_user()
+    flash('You have been successfully logged out')
+    return redirect(url_for("main.index"))
+
+
+@auth.route('/register',methods = ["GET","POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email = form.email.data, username = form.username.data,password = form.password.data)
+        db.session.add(user)
+        db.session.commit()
         return redirect(url_for('auth.login'))
-    return render_template('auth/logout.html',log_out= login_form)
+        title = "New Account"
+    return render_template('auth/register.html',registration_form = form)
