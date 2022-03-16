a19e5426b59b7b22202f4df1d536cfbb8e2cfae3
MARTIN OMOLLO <martin.ochieng@student.moringaschool.com>
2022-03-17 00:14:58 +0300

    add text

--- start.sh
+++ start.sh
@@ -2,5 +2,5 @@ export SECRET_KEY='pitchmartin'
 
 export MAIL_USERNAME='martinochieng1999@gmail.com'
 export MAIL_PASSWORD='MARtin1999@'
-export SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://martin:martin123@localhost/pitches'
+# export SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://martin:martin123@localhost/pitches'
 python3 manage.py server
